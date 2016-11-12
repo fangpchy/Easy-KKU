803217131bdaa18f21ba7592cc7c3babb0c0e205
fangpchy <fangpchy@gmail.com>
2016-11-12 12:04:47 +0700

    Intent to SignUp finish

--- app/src/main/java/com/example/fangpchy/easykku/MainActivity.java
+++ app/src/main/java/com/example/fangpchy/easykku/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.fangpchy.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit(ประกาศตัวแปร)
+    //มี 3 ส่วน 1.Access -private,public 2.Type 3.Name
+    private Button signInButton, signUpButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //การทำ Bind widget(การผูก widget กับตัวแปร)
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button);
+
+        //การเขียน Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    } // Main Method
+
+} // Main Class นี่คือ คลาสหลัก
