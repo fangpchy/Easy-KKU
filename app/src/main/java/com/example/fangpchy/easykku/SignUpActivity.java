176a48b2ce5ac4743909f07bed69e40afe954435
fangpchy <fangpchy@gmail.com>
2016-11-12 16:00:24 +0700

    Read Path OK

--- app/src/main/java/com/example/fangpchy/easykku/SignUpActivity.java
+++ app/src/main/java/com/example/fangpchy/easykku/SignUpActivity.java
@@ -1,10 +1,12 @@
 package com.example.fangpchy.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,7 +21,8 @@ public class SignUpActivity extends AppCompatActivity {
             userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString,
+            imagePathString, imageNameString;
     private Uri uri;
 
 
@@ -103,8 +106,33 @@ public class SignUpActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+            //การหา Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1", "imagepath ==>" + imagePathString);
+
+
+
 
         }   // if
 
     }   //OnActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null ,null, null);
+
+        if (cursor != null) {
+
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+
+        } else {
+            result = uri.getPath();
+        }
+
+        return null;
+    }
 }   // Main Class
