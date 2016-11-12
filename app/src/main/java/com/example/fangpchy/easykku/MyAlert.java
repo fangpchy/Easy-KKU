10ba6207547e21df89e199a74e69a56c5ffdb681
fangpchy <fangpchy@gmail.com>
2016-11-12 14:25:05 +0700

    Check Have Space

---
+++ app/src/main/java/com/example/fangpchy/easykku/MyAlert.java
@@ -0,0 +1,18 @@
+package com.example.fangpchy.easykku;
+
+import android.content.Context;
+
+/**
+ * Created by User on 12/11/2559.
+ */
+
+public class MyAlert {
+
+    //ประกาศตัวแปร
+    private Context context;
+    private int anInt;
+    private String titleString, messangeString;
+
+
+
+} //Main Class
